534285c3340b2d74eafb92348ab7970b523913a5
yasaruzun <yasaruzun91@gmail.com>
2022-11-05 12:13:26 +0300

    05.11.2022

---
+++ src/ArraydekiTumElemanları2Arttirma.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class ArraydekiTumElemanları2Arttirma {
+    public static void main(String[] args) {
+
+    //Soru 1- Verilen bir int array’in tum elemanlarini 2 artirip bize donduren bir method
+    //olusturun. Eski array’i yeni haliyle kaydedin.
+
+
+        int[] arr = {2, 4, 6, 8, 10};
+        arr=tumElemanlari2Arttir(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    public static int [] tumElemanlari2Arttir (int[] arr){
+
+        for (int i = 0; i < arr.length; i++) {
+
+            arr[i]=arr[i]+2;
+
+
+        }
+        return arr;
+    }
+}
+
