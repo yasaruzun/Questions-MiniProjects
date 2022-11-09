cd7e6c5ede873f04412aa7392e0d7f48c856d6d1
yasaruzun <yasaruzun91@gmail.com>
2022-11-09 13:01:30 +0300

    09.11.2022

---
+++ src/XyzDizimiTrueFalseKontrol.java
@@ -0,0 +1,29 @@
+public class XyzDizimiTrueFalseKontrol {
+
+    public static void main(String[] args) {
+        /*
+         * girilen String icinde "xyz" dizimi var ise true degilse false return eden metod yaziniz
+         *
+         * input      output
+         * axyzm  ==   true
+         * xyz    ==   true
+         * x.yz   ==   false
+         * xyaz   ==   false
+         */
+
+
+        String input="ahmetxyz";
+
+        boolean dogruMu=false;
+
+        if(input.contains("xyz")){
+
+            dogruMu=true;
+
+
+        }
+
+        System.out.println(dogruMu);
+
+    }
+}
