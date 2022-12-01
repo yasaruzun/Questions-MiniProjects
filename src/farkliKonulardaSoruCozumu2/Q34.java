61374b30af74c487a3e801c57388f1424c9b068a
yasaruzun <yasaruzun91@gmail.com>
2022-12-01 14:46:08 +0300

    01.12.2022

--- src/farkliKonulardaSoruCozumu2/Q34.java
+++ src/farkliKonulardaSoruCozumu2/Q34.java
@@ -1,5 +1,9 @@
 package farkliKonulardaSoruCozumu2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Q34 {
      /*
         Bir tamsayı dizisinde tüm sıfırları sona koyan bir program create ediniz.
@@ -9,6 +13,21 @@ public class Q34 {
 
     public static void main(String[] args) {
 
+        int[] sayi = {-1, 0, 4, 2, 0};
+        int[] sayi1 = new int[sayi.length];
+        int index = 0;
+        for (int i = 0; i < sayi.length; i++) {
+
+            if (sayi[i] != 0) {
+                sayi1[index] = sayi[i];
+                index++;
+            }
+
+        }
+
+        System.out.println(Arrays.toString(sayi1));
 
     }
+
 }
+
